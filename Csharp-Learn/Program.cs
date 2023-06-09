929b23e2bd6df3de84d94d62a3ae4584bb135a80
JaoharRaihan <jaoharraihan@gmail.com>
2023-06-10 02:00:38 +0600

    s

--- Csharp-Learn/Program.cs
+++ Csharp-Learn/Program.cs
@@ -10,8 +10,33 @@ namespace Csharp_Learn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Good");
+            // Singleline Comment
+            /*
+             * Multiline comment
+             */
+            //int Raihan = 33; //Integer Variable
+            /* Data types in C#;
+             * Integer - int Raihan;--  4 bytes
+             * Long - long Raihan;-- 8 bytes
+             * Floating point number - float that;-- 2 bytes
+             * Double - double Raihan; -- 8 bytes
+             * Character - Char a='A';-- 2 bytes
+             * Boolean - bool is Great =true;-- 1bit
+             * String inp="Raihan";-- 2 bytes per character
+             */
+            //string inp = Console.ReadLine();
+            //Console.WriteLine(inp);
+            /*
+            Console.WriteLine("How are You?");
+            Console.WriteLine("Good"+Raihan);
+            Console.WriteLine("Ok");
+            */
+            int S = 33;
+            float A = 33F;
+            double R = 33D;
+            Console.WriteLine(S);
+            Console.WriteLine(A);
+            Console.WriteLine(R);
             Console.ReadLine();
         }
     }
