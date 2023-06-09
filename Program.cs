e39672504b540e23ff41af4e5363255d0d6c5f9a
JaoharRaihan <jaoharraihan@gmail.com>
2023-06-09 16:20:54 +0600

    s

---
+++ Program.cs
@@ -0,0 +1,17 @@
+USing system;
+USing system.collections.Generic;
+Using system.liq;
+using system.text;
+using system.threading.Tasks;
+
+namespace Hello
+{
+    Class program
+    {
+        static void Main(string[] args)
+        {
+            console.writeLine("Hello World");
+            console.Readline();
+        }
+    }
+}
